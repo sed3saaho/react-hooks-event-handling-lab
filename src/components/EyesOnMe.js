bec95ccf18dac9924a59515383a2fc13edce08e1
Sedly <sedly.keith@student.moringaschool.com>
2024-04-26 11:55:54 +0300

    react-hooks-event-handling-lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
+import React, { useState } from 'react';
 // Code EyesOnMe Component Here
+
+
+
+function EyesOnMe() {
+    const handleFocus = () => {
+      console.log('Good!');
+    };
+  
+    const handleBlur = () => {
+      console.log('Hey! Eyes on me!');
+    };
+  
+    return (
+      <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>
+          Eyes on me
+        </button>
+      </div>
+    );
+  }
+  
+  export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    const [password, setPassword] = useState('');
+  
+    const handleChange = (event) => {
+      setPassword(event.target.value);
+      console.log('Entering password...');
+    };
+  
     return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+      <div>
+        <input type="password" value={password} onChange={handleChange} />
+      </div>
+    );
+  }
+  
+  export default Keypad;
